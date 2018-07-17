5162acece11e5e25e23082f0ce324562b20d7d4f
bartool <bartoolina@gmail.com>
2018-07-17 18:38:40 +0200

    Cleannig code, chnage names of variables

--- NumbersToString/Program.cs
+++ NumbersToString/Program.cs
@@ -19,24 +19,24 @@ namespace NumbersToString
             ends = new string[] { "nascie", "dziescia", "dziesci", "dziesiat", "sta", "set" };
         }
 
-        private string MojaFunkcja(int[] testNumber)
+        private string ConvertNumberToString(int[] testNumber)
         {
             string result = "";
             for (int i = 0; i < testNumber.Length; i++)
             {
-                result += ConvertNumberToString(testNumber[i], (number)(testNumber.Length - i - 1)) + " ";
+                result += _ConvertNumberToString(testNumber[i], (number)(testNumber.Length - i - 1)) + " ";
             }
             return result;
         }
-        private string ConvertNumberToString (int number, number nr)
+        private string _ConvertNumberToString (int number, number nr)
         {
-            string temp = units[number];
+            string tempData = units[number];
             switch (nr)
             {
                 case Program.number.unit:
                     if (test)
                     {
-                        temp += ends[0];
+                        tempData += ends[0];
                         test = false;
                     }                  
                     break;
@@ -45,11 +45,11 @@ namespace NumbersToString
                     {
                         switch (number)
                         {
-                            case 1:     test = true; temp = ""; break; 
-                            case 2:     temp += ends[1]; break;
+                            case 1:     test = true; tempData = ""; break; 
+                            case 2:     tempData += ends[1]; break;
                             case 3: 
-                            case 4:     temp += ends[2]; break;
-                            default:    temp += ends[3]; break;
+                            case 4:     tempData += ends[2]; break;
+                            default:    tempData += ends[3]; break;
                         }
                         break;
                     }
@@ -57,17 +57,17 @@ namespace NumbersToString
                     {
                         switch (number)
                         {
-                            case 1:     temp = "sto"; break;
-                            case 2:     temp = "dwiescie"; break;
+                            case 1:     tempData = "sto"; break;
+                            case 2:     tempData = "dwiescie"; break;
                             case 3:
-                            case 4:     temp += ends[4]; break;
-                            default:    temp += ends[5]; break;                      
+                            case 4:     tempData += ends[4]; break;
+                            default:    tempData += ends[5]; break;                      
                         }
                         break;
                     }
                 default: break;
             }
-            return temp;
+            return tempData;
         }
         static void Main(string[] args)
         {
@@ -75,12 +75,12 @@ namespace NumbersToString
             do
             {
                 Console.Write("Podaj liczbe w zakresie 0-999: ");
-                string a3 = Console.ReadLine();
+                string inputString = Console.ReadLine();
 
-                int[] a4 = a3.Select(n => Convert.ToInt32(n) - '0').ToArray();
+                int[] inputInt = inputString.Select(n => Convert.ToInt32(n) - '0').ToArray();
                 
                 Console.WriteLine();
-                try { Console.WriteLine(testuje.MojaFunkcja(a4)); }
+                try { Console.WriteLine(testuje.ConvertNumberToString(inputInt)); }
                 catch { Console.WriteLine("Czy Ty wiesz czym sie rożnią liczby od znaków?"); }
                 Console.Write("Jeszcze raz?");
             } while ("t" == Console.ReadLine());
